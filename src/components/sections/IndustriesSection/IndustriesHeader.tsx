2426a5f8c8f094b9c6f1ca7987890178288b4ed0
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-07-16 08:21:09 +0300

    fix-css

--- src/components/sections/IndustriesSection/IndustriesHeader.tsx
+++ src/components/sections/IndustriesSection/IndustriesHeader.tsx
@@ -1,14 +1,18 @@
+'use client'
+
 import styles from './IndustriesSection.module.css'
 
 export const IndustriesHeader = ({ casesCount }: { casesCount: number }) => (
   <header className={styles.header}>
     <span className={styles.expertise}>EXPERTISE</span>
     <h2 className={styles.title}>
-      <span className={styles.industries}>INDUSTRIES</span>
-      <span className={styles.weKnowWell}>
-        WE KNOW WELL
-        <span className={styles.casesCount}>({casesCount})</span>
-      </span>
+      <div className={styles.industriesBadge}>
+        <span className={styles.industries}>INDUSTRIES</span>
+        <span className={styles.weKnowWell}>
+          WE KNOW WELL
+          <span className={styles.casesCount}>({casesCount})</span>
+        </span>
+      </div>
     </h2>
   </header>
 )
